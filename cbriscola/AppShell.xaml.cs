6511e6de3488d2c2a4f80b0340cbf7e9b00fd9b7
numer <numer@NUMERONESOFT>
2023-01-08 05:05:21 +0100

    Aggiungere i file di progetto.

---
+++ cbriscola/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace cbriscola;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ cbriscola/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace cbriscola;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
